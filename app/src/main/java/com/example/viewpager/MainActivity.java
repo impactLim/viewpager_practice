ef843b9d0dd9ae62fa6299477afc2d50a6319a36
impactLim <impacts.lab@gmail.com>
2020-05-27 02:13:56 +0900

    viewpager

---
+++ app/src/main/java/com/example/viewpager/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
+
+        // ViewPager와  FragmentAdapter 연결
+        viewPager.setAdapter(fragmentAdapter);
+
+        // Fragment로 넘길 Image Resource
+        ArrayList<Integer> listImage = new ArrayList<>();
+        listImage.add(R.drawable.first);
+        listImage.add(R.drawable.second);
+        listImage.add(R.drawable.third);
+        listImage.add(R.drawable.fourth);
+        listImage.add(R.drawable.fifth);
+
+//        // FragmentAdapter에 Fragment 추가, Image 개수만큼 추가
+//        for (int i = 0; i < 4; i++) {
+//            ImageFragment imageFragment = new ImageFragment();
+//            fragmentAdapter.addItem(imageFragment);
+//        }
+//
+//        fragmentAdapter.notifyDataSetChanged();
+
+
+        viewPager.setClipToPadding(false);
+        int dpValue = 16;
+        float d = getResources().getDisplayMetrics().density;
+        int margin = (int) (dpValue * d);
+        viewPager.setPadding(margin, 0, margin, 0);
+        viewPager.setPageMargin(margin/2);
+
+        // FragmentAdapter에 Fragment 추가, Image 개수만큼 추가
+        for (int i = 0; i < listImage.size(); i++) {
+            ImageFragment imageFragment = new ImageFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("imgRes", listImage.get(i));
+            imageFragment.setArguments(bundle);
+            fragmentAdapter.addItem(imageFragment);
+        }
+        fragmentAdapter.notifyDataSetChanged();
+
+
+
+        // gif 3초 이미지뷰
+        ImageView rabbit_image = (ImageView) findViewById(R.id.imageView);
+        Glide.with(this).load(R.drawable.splashgif).into(rabbit_image);
+
+    }
+
+    class FragmentAdapter extends FragmentPagerAdapter {
+
+        // ViewPager에 들어갈 Fragment들을 담을 리스트
+        private ArrayList<Fragment> fragments = new ArrayList<>();
+
+        // 필수 생성자
+        FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        // List에 Fragment를 담을 함수
+        void addItem(Fragment fragment) {
+            fragments.add(fragment);
+        }
+    }
+}
